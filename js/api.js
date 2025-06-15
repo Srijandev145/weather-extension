8ecdf8469d8143f59a9fca573f867080931a67fc
Srijandev145 <srijandsofficial@gmail.com>
2025-06-16 01:39:45 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 export async function fetchWeather(lat, lon) {
     try {
-        const response = await fetch(`http://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civillight&output=json`);
+        const response = await fetch(`https://www.7timer.info/bin/api.pl?lon=${lon}&lat=${lat}&product=civillight&output=json`);
         if (!response.ok) throw new Error('Failed to fetch weather data');
         return await response.json();
     } catch (error) {
